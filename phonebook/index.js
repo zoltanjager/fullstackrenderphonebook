6a8e1fe10a1cc6a0ac6bfc08b51c669aefa1bc65
zoltanjager <zoltan.jager@gmail.com>
2024-12-17 14:05:29 +0100

    Application to the Internet, add environment var

--- phonebook/index.js
+++ phonebook/index.js
@@ -104,6 +104,7 @@ app.post('/api/persons', (request, response) => {
   
 })
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
